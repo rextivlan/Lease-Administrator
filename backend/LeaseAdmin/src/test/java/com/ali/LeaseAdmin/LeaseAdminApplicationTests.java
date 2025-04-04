588c3264efab943016adabbc64716b0533d90428
rextivlan <affan.ali368@gmail.com>
2025-04-04 00:25:02 -0400

    Initial Commit - Spring Initializr

---
+++ backend/LeaseAdmin/src/test/java/com/ali/LeaseAdmin/LeaseAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ali.LeaseAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaseAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
